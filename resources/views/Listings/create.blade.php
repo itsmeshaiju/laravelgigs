2cae0408c4f5006baac72f9cca2dcae8729433d9
Shaiju PM <shaiju.pm@infolitz.com>
2023-06-15 15:44:37 +0530

    keep old data using old

--- resources/views/Listings/create.blade.php
+++ resources/views/Listings/create.blade.php
@@ -18,7 +18,7 @@
                             <input
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
-                                name="company"
+                                name="company" value="{{old('company')}}"
                             />
                             @error('company')
                                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -34,7 +34,7 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="title"
                                 placeholder="Example: Senior Laravel Developer"
-                            />
+                             value="{{old('title')}}"/>
                             @error('title')
                                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                             @enderror
@@ -49,7 +49,7 @@
                             <input
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
-                                name="location"
+                                name="location" value="{{old('location')}}"
                                 placeholder="Example: Remote, Boston MA, etc"
                             />
                             @error('location')
@@ -64,7 +64,7 @@
                             <input
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
-                                name="email"
+                                name="email" value="{{old('email')}}"
                             />
                             @error('email')
                                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -81,7 +81,7 @@
                             <input
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
-                                name="website"
+                                name="website" value="{{old('website')}}"
                             />
                             @error('website')
                                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -95,7 +95,7 @@
                             <input
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
-                                name="tags"
+                                name="tags" value="{{old('tags')}}"
                                 placeholder="Example: Laravel, Backend, Postgres, etc"
                             />
                             @error('tags')
@@ -126,7 +126,7 @@
                                 name="description"
                                 rows="10"
                                 placeholder="Include tasks, requirements, salary, etc"
-                            ></textarea>
+                            >{{old('description')}}</textarea>
                             @error('description')
                                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                             @enderror
