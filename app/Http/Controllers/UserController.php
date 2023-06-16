63484ca70f89e1df24436cd85414718cc8f06f48
Shaiju PM <shaiju.pm@infolitz.com>
2023-06-16 11:40:56 +0530

    user registration added

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    //User Form create
+    public function create(){
+        return view('User.register');
+    }
+
+    //Store User data to db
+    public function store(Request $request){
+        $formFields = $request->validate(
+            [
+            'name'=>'required','min:3',
+            'email'=>'required','email',Rule::unique('users','email'),
+            'password'=>'required|confirmed|min:6']
+        );
+
+        //Hash Password
+        $formFields['password']=bcrypt($formFields['password']);
+
+        //Crate User
+        $user= User::create($formFields);
+        //login user
+        Auth()->login($user);
+        return redirect('/')->with('message','User created and logged in');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -39,6 +40,11 @@ Route::put('listings/{listing}',[ListingController::class,'update']);
 //Delete Listing
 Route::delete('listings/{listing}',[ListingController::class,'destory']);
 
+//User Registration Form
+Route::get('register',[UserController::class,'create']);
+
+//User Storing To DB
+Route::post('users',[UserController::class,'store']);
 // Route::get('hello',function(){
 //     return response('Hello World');
 // });
